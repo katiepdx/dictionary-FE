6b439f3ab7924212c8ab8158ed22835dc44230dc
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-30 18:28:13 -0700

    remove extra details from WordItem

--- src/components/app/displays/word-item/WordItem.jsx
+++ src/components/app/displays/word-item/WordItem.jsx
@@ -2,42 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './WordItem.css'
 
-const WordItem = ({
-  word,
-  wordLanguage,
-  wordTranslation,
-  wordDefinition,
-  exampleSentence,
-  notes }) => {
+const WordItem = ({ word }) => {
   return (
     <div className={styles.wordItem}>
       <h4>Word: {word}</h4>
-
-      <p className={styles.label}>Language</p>
-      <p> {wordLanguage}</p>
-
-      <p className={styles.label}>Translation</p>
-      <p> {wordTranslation ? wordTranslation : 'n/a'}</p>
-
-      <p className={styles.label}>Definition</p>
-      <p> {wordDefinition}</p>
-
-      <p className={styles.label}>Example Sentence</p>
-      <p> {exampleSentence}</p>
-
-      <p className={styles.label}>Notes</p>
-      <p> {notes ? notes : 'n/a'}</p>
     </div>
   )
 }
 
 WordItem.propTypes = {
-  word: PropTypes.string.isRequired,
-  wordLanguage: PropTypes.string.isRequired,
-  wordTranslation: PropTypes.string,
-  wordDefinition: PropTypes.string.isRequired,
-  exampleSentence: PropTypes.string.isRequired,
-  notes: PropTypes.string
+  word: PropTypes.string.isRequired
 }
 
 export default WordItem
