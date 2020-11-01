aa6ae79c6942df11c714257817c3937fb69bef6b
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-31 18:02:47 -0700

    update tests

--- src/components/app/displays/word-details/WordDetails.test.jsx
+++ src/components/app/displays/word-details/WordDetails.test.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import WordDetails from './WordDetails'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('<WordDetails/>', () => {
   it('should render the word details to the page', () => {
-    const { asFragment } = render(<WordDetails
+    const { asFragment } = render(
+    <MemoryRouter>
+      <WordDetails
+      id={5}
       word="WordDetailTest"
       wordLanguage="WordDetailTest"
       wordTranslation="WordDetailTest"
       wordDefinition="WordDetailTest"
       exampleSentence="WordDetailTest"
       notes="I learned a new word!"
-    />)
+    /></MemoryRouter>)
 
     expect(asFragment()).toMatchSnapshot()
   })
